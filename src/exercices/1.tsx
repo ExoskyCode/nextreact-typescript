cf9079d69d24d713e9252b153a233d694912f5d6
ExoskyCode <exoskyv2@gmail.com>
2023-03-23 09:32:33 +0100

    Ex 1 Part 1

--- src/exercices/1.tsx
+++ src/exercices/1.tsx
@@ -1,38 +1,40 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/prefer-ts-expect-error */
 import clsx from 'clsx';
+import { ComponentPropsWithoutRef } from 'react';
 
 // 🦁 Supprime ce commentaire et définit correctement les types pour ce composant
-type SquareProps = any;
+type SquareProps = {
+	isWinningSquare: boolean;
+} & ComponentPropsWithoutRef<'button'>;
 
-const Square = (props: SquareProps) => {
-  // 🦁 Remplace ça par les props définit en haut
-  return (
-    <button
-      className={clsx('square', {
-        'winning-square': false, // 🦁 Remplace ça par la prop isWinningSquare
-      })}
-    >
-      O {/* 🦁 Remplace ça par la prop children */}
-    </button>
-  );
+const Square = ({ isWinningSquare, children, ...props }: SquareProps) => {
+	return (
+		<button
+			className={clsx('square', {
+				'winning-square': { isWinningSquare },
+			})}
+			{...props}>
+			{children}
+		</button>
+	);
 };
 
 const Game = () => {
-  return (
-    <div className="game">
-      <Square isWinningSquare={true}>X</Square>
-      <Square isWinningSquare={false}>X</Square>
-      <Square isWinningSquare={true}>O</Square>
-    </div>
-  );
+	return (
+		<div className='game'>
+			<Square isWinningSquare={true}>X</Square>
+			<Square isWinningSquare={false}>X</Square>
+			<Square isWinningSquare={true}>O</Square>
+		</div>
+	);
 };
 
 export default function App() {
-  return (
-    <div>
-      <h2>TicTacToe</h2>
-      <Game />
-    </div>
-  );
+	return (
+		<div>
+			<h2>TicTacToe</h2>
+			<Game />
+		</div>
+	);
 }
